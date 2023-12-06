fd316b17135e5bdf7f3b3ac47ddc3fdbefce4211
Rigbay <malikhilman@outlook.com>
2023-12-06 10:49:39 +0100

    Add project files.

---
+++ TypecursusApplicatie/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace TypecursusApplicatie
+{
+    public partial class App : Application
+    {
+        // De implementatie van de App class.
+    }
+}
---
+++ TypecursusApplicatie/User Interface/Inlogpagina.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace TypecursusApplicatie
+{
+    public partial class Inlogpagina : Window
+    {
+        public Inlogpagina()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Implementeer inloglogica hier
+        }
+
+        private void SidebarToggle_Click(object sender, RoutedEventArgs e)
+        {
+            // Toggle de breedte van de sidebar
+            Sidebar.Width = Sidebar.Width == 0 ? 200 : 0;
+        }
+    }
+}
